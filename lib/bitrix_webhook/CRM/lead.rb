3f9b86163ed990a96559471755acfd151dbd28fb
Serhii Danovskyi <jipebed@gmail.com>
2018-06-22 11:05:53 +0300

    fix user

--- lib/bitrix_webhook/CRM/lead.rb
+++ lib/bitrix_webhook/CRM/lead.rb
@@ -2,30 +2,32 @@ module BitrixWebhook
   module CRM
     module LEAD
 
-      DEFAULT = {
-          fname: '',
-          lname: '',
-          status_id: 'NEW',
-          opened:  'Y',
-          assigned_by_id: BitrixWebhook.webhook_user,
-          register_sonet_event: 'Y',
-      }
+      def self.config
+        {
+            fname: '',
+            lname: '',
+            status_id: 'NEW',
+            opened:  'Y',
+            assigned_by_id: BitrixWebhook.webhook_user,
+            register_sonet_event: 'Y',
+        }
+      end
 
       def self.base_url(method)
         "https://#{BitrixWebhook.bitrix24_url}/rest/#{BitrixWebhook.webhook_user}/#{ BitrixWebhook.hook}/crm.lead.#{method}.json?"
       end
 
       def self.add(options = {})
-        options = DEFAULT.merge(options )
+        options = config.merge(options )
         post_url =  base_url("add").to_s + "fields%5BTITLE%5D=#{options[:fname].to_s + '+' + options[:lname].to_s}&" +
-                                                    "fields%5BNAME%5D=#{options[:fname]}&" +
-                                                    "fields%5BLAST_NAME%5D=#{options[:lname]}&" +
-                                                    "fields%5BSTATUS_ID%5D=#{options[:status_id]}&" +
-                                                    "fields%5BOPENED%5D=#{options[:opened]}&" +
-                                                    "fields%5BASSIGNED_BY_ID%5D=#{options[:assigned_by_id]}&" +
-                                                    "fields%5BPHONE%5D%5B0%5D%5BVALUE%5D=#{options[:phone]}&" +
-                                                    "fields%5BEMAIL%5D%5B0%5D%5BVALUE%5D=#{options[:email]}&" +
-                                                    "params%5BREGISTER_SONET_EVENT%5D=#{options[:register_sonet_event]}"
+            "fields%5BNAME%5D=#{options[:fname]}&" +
+            "fields%5BLAST_NAME%5D=#{options[:lname]}&" +
+            "fields%5BSTATUS_ID%5D=#{options[:status_id]}&" +
+            "fields%5BOPENED%5D=#{options[:opened]}&" +
+            "fields%5BASSIGNED_BY_ID%5D=#{options[:assigned_by_id]}&" +
+            "fields%5BPHONE%5D%5B0%5D%5BVALUE%5D=#{options[:phone]}&" +
+            "fields%5BEMAIL%5D%5B0%5D%5BVALUE%5D=#{options[:email]}&" +
+            "params%5BREGISTER_SONET_EVENT%5D=#{options[:register_sonet_event]}"
 
         begin
           JSON.parse(HTTP.post(post_url).body)
