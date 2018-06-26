35e21949a5a15b14d22b8e74d0af2d73fb871b20
Isaiev <webisaev@gmail.com>
2018-06-26 12:40:48 +0300

    use define_singleton_method instead of custom method

--- lib/helpers/configuration.rb
+++ lib/helpers/configuration.rb
@@ -7,20 +7,12 @@ module Configuration
   def define_setting(name, default = nil)
     class_variable_set("@@#{name}", default)
 
-    define_class_method "#{name}=" do |value|
+    define_singleton_method("#{name}=") do |value|
       class_variable_set("@@#{name}", value)
     end
 
-    define_class_method name do
+    define_singleton_method(name) do
       class_variable_get("@@#{name}")
-    end
-  end
-
-  private
-
-  def define_class_method(name, &block)
-    (class << self; self; end).instance_eval do
-      define_method name, &block
     end
   end
 
